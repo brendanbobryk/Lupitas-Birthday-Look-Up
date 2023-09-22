e21eacdc3bc8b06daa323ceab9fed0e33e7c5c1c
Brendan Bobryk <babobryk@gmail.com>
2023-09-22 16:57:45 -0700

    Project setup

---
+++ lupitasbirthdaylookup/src/main/java/com/cis084101javaprogramming/App.java
@@ -0,0 +1,15 @@
+package com.cis084101javaprogramming;
+
+/**
+ * Name: Brendan Bobryk
+ * Student ID #: 1036738
+ * Class: CIS 084 Java Programming
+ * Date: 10/3/23
+ * Program: Lupita's Birthday Look Up
+ */
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
